f4b414fc45576ba9b9ef7b300f11503469c8c2a7
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-12-21 10:31:31 +0100

    Updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,65 +2,41 @@
 """Log parsing"""
 import sys
 
+def process_line(line, cache, total_size, count):
+    line_list = line.split(" ")
+    if len(line_list) > 4:
+        code = line_list[-2]
+        size = int(line_list[-1])
+        if code in cache:
+            cache[code] += 1
+        total_size += size
+        count += 1
+    return total_size, count
 
-def print_stats(total_size, status_codes):
-    """
-    Print statistics including total file size and
-    number of lines for each status code.
-    """
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes):
-        print("{}: {}".format(code, status_codes[code]))
-
-
-def process_line(line, total_size, status_codes):
-    """
-    Process a log entry line, update total file size and status code counts.
-    """
-    try:
-        parts = line.split()
-        if len(parts) == 9:
-            status_code = int(parts[-2])
-            file_size = int(parts[-1])
-            total_size += file_size
-            if status_code in {200, 301, 400, 401, 403, 404, 405, 500}:
-                if status_code not in status_codes:
-                    status_codes[status_code] = 1
-                else:
-                    status_codes[status_code] += 1
-        return total_size, status_codes
-    except ValueError:
-        return total_size, status_codes
-
+def print_stats(total_size, cache):
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
 
 def main():
-    """
-    Main function to read stdin line by line,
-    compute metrics, and print statistics.
-    """
+    cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+             '403': 0, '404': 0, '405': 0, '500': 0}
     total_size = 0
-    status_codes = {}
-    line_count = 0
+    count = 0
 
     try:
         for line in sys.stdin:
-            line = line.strip()
-            total_size, status_codes = process_line(
-                line,
-                total_size,
-                status_codes
-                )
-            line_count += 1
+            total_size, count = process_line(line, cache, total_size, count)
+            if count == 10:
+                count = 0
+                print_stats(total_size, cache)
 
-            if line_count == 10:
-                print_stats(total_size, status_codes)
-                line_count = 0
-
-    except KeyboardInterrupt:
+    except Exception as err:
         pass
 
-    print_stats(total_size, status_codes)
-
+    finally:
+        print_stats(total_size, cache)
 
 if __name__ == "__main__":
     main()
