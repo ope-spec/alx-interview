0c05bba495f06238b38ee8a49b67467189eecbde
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-12-21 10:53:11 +0100

    Updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,49 @@
 #!/usr/bin/python3
-"""Log parsing"""
+"""script that reads stdin line by line and computes metrics"""
 import sys
 
-def process_line(line, cache, total_size, count):
-    line_list = line.split(" ")
-    if len(line_list) > 4:
-        code = line_list[-2]
-        size = int(line_list[-1])
-        if code in cache:
-            cache[code] += 1
-        total_size += size
-        count += 1
-    return total_size, count
+# Initialize a dictionary to store counts of different result codes
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 
-def print_stats(total_size, cache):
+# Initialize variables to track total file size and line count
+total_size = 0
+line_count = 0
+
+try:
+    # Iterate over each line from standard input
+    for line in sys.stdin:
+        # Split the line into a list of words
+        line_list = line.split(" ")
+
+        # Check if the line has the expected format
+        if len(line_list) > 4:
+            result_code = line_list[-2]
+            size = int(line_list[-1])
+
+            # Update the count for the result code in the dictionary
+            if result_code in cache.keys():
+                cache[result_code] += 1
+
+            # Update total file size and line count
+            total_size += size
+            line_count += 1
+
+        # Print statistics after every 10 lines
+        if line_count == 10:
+            line_count = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    # Ignore exceptions and continue to finally block
+    pass
+
+finally:
+    # Print final statistics
     print('File size: {}'.format(total_size))
     for key, value in sorted(cache.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
-
-def main():
-    cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-             '403': 0, '404': 0, '405': 0, '500': 0}
-    total_size = 0
-    count = 0
-
-    try:
-        for line in sys.stdin:
-            total_size, count = process_line(line, cache, total_size, count)
-            if count == 10:
-                count = 0
-                print_stats(total_size, cache)
-
-    except Exception as err:
-        pass
-
-    finally:
-        print_stats(total_size, cache)
-
-if __name__ == "__main__":
-    main()
