89e4594d6bdaa1e726ce1cdb3290abfde2792715
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2024-01-18 14:43:23 +0100

    Updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,41 @@
-#!/usr/bin/node
+#!/path/to/your/node
 const request = require('request');
 
-const movieId = process.argv[2];
+const apiUrl = 'https://swapi-api.hbtn.io/api';
 
-if (!movieId) {
+if (process.argv.length <= 2) {
   console.error('Please provide the Movie ID as a command-line argument.');
   process.exit(1);
 }
 
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+const movieId = process.argv[2];
 
-request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
+request(`${apiUrl}/films/${movieId}/`, (err, _, body) => {
+  if (err) {
+    console.error('Error fetching film data:', err);
     process.exit(1);
   }
 
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch data. Status Code:', response.statusCode);
-    process.exit(1);
-  }
+  const charactersURL = JSON.parse(body).characters;
 
-  const filmData = JSON.parse(body);
-
-  const characters = filmData.characters;
-
-  characters.forEach(characterUrl => {
-    request(characterUrl, function (error, response, characterBody) {
-      if (error) {
-        console.error('Error:', error);
-        process.exit(1);
+  const getCharacterName = url => new Promise((resolve, reject) => {
+    request(url, (requestErr, __, charactersReqBody) => {
+      if (requestErr) {
+        reject(requestErr);
+      } else {
+        resolve(JSON.parse(charactersReqBody).name);
       }
-
-      if (response.statusCode !== 200) {
-        console.error('Failed to fetch character data. Status Code:', response.statusCode);
-        process.exit(1);
-      }
-
-      const characterData = JSON.parse(characterBody);
-      console.log(characterData.name);
     });
   });
+
+  const charactersNamePromises = charactersURL.map(getCharacterName);
+
+  Promise.all(charactersNamePromises)
+    .then(names => {
+      console.log(names.join('\n'));
+    })
+    .catch(allErr => {
+      console.error('Error fetching character names:', allErr);
+      process.exit(1);
+    });
 });
