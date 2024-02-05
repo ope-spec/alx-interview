4787d8c1446b70e6df9f5f6225da93b49aaa5fc1
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2024-02-05 09:25:54 +0100

    Task done

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Function to calculate the perimeter of the island
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
