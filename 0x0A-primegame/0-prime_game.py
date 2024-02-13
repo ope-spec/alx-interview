02236846d5473811c373316d60da62324911a55f
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2024-02-13 09:15:06 +0100

    Updated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+Prime Game
+"""
+
 
 def is_prime(num):
+    """
+    Check if a number is prime.
+
+    Parameters:
+    num (int): The number to check for primality.
+
+    Returns:
+    bool: True if the number is prime, False otherwise.
+    """
     if num <= 1:
         return False
     if num <= 3:
@@ -16,6 +29,15 @@ def is_prime(num):
 
 
 def prime_game_winner(nums):
+    """
+    Determine the winner of a prime number game.
+    Parameters:
+    nums (list of int): A list of numbers representing
+    the choices made by the players.
+    Returns:
+    str or None: The name of the winner ("Maria" or "Ben"),
+    or None if it's a tie.
+    """
     primes = [num for num in range(2, max(nums) + 1) if is_prime(num)]
     wins_maria = 0
     wins_ben = 0
@@ -36,12 +58,10 @@ def prime_game_winner(nums):
 
 
 def isWinner(x, nums):
+    """
+    Determine the winner of a prime number game.
+    Returns:
+    str or None: The name of the winner ("Maria" or "Ben"),
+    or None if it's a tie.
+    """
     return prime_game_winner(nums)
-
-
-if __name__ == "__main__":
-    winner = isWinner(5, [2, 5, 1, 4, 3])
-    if winner:
-        print("Winner:", winner)
-    else:
-        print("Winner: None")
